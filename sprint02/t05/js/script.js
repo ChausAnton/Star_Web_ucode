2970d2f87671f434316a511db65d7da3b0ee11a3
Anton Chaus <anchaus@c1r6p8.khpi.ucode-connect.study>
2021-03-08 16:19:39 +0200

    fix t05

--- sprint02/t05/js/script.js
+++ sprint02/t05/js/script.js
@@ -1,9 +1,10 @@
 function total(addCount=0, addPrice=0, currentTotal=0) {
-    addCount = typeof addCount == 'number' ? addCount : 0;
-    addPrice = typeof addPrice == 'number' ? addPrice : 0;
-    currentTotal = typeof currentTotal == 'number' ? currentTotal : 0;
-    
-    let prise = (addCount * addPrice) + currentTotal;
 
-    return prise.toFixed(2);
+    if(isFinite(addCount) == true && isFinite(addPrice) == true 
+    && isFinite(currentTotal) == true) {
+        let prise = (addCount * addPrice) + currentTotal;
+        return prise.toFixed(2);
+    }
+    return 0;
+
 }
