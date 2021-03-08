6fc97ef87ffe24e0938b24d2b5e7cb1f2d3a396d
Anton Chaus <anchaus@c1r6p8.khpi.ucode-connect.study>
2021-03-08 13:04:49 +0200

    t01

--- sprint02/t00/js/script.js
+++ sprint02/t00/js/script.js
@@ -1,3 +1,3 @@
-function outside() {
     alert("Hello JavaScript from outside")
-}
+    /*The alert() method displays an alert box with a specified message and an OK button. An alert box is often used if you want to make sure information comes through to the user. Note: 
+    The alert box takes the focus away from the current window, and forces the browser to read the message.*/
---
+++ sprint02/t01/js/script.js
@@ -0,0 +1,20 @@
+
+var Nummber = 1;
+var BigInt = 9007199254740991n;
+var string = "hello";
+var Boolean = true;
+let Null = null;
+var undefined;
+var object = {};
+var symbol = Symbol("foo");
+var myFunction = function (a, b) {return a * b};
+
+alert("Nummber is " + typeof(Nummber) + "\n" +
+"BigInt is " + typeof(BigInt) + "\n" +
+"string is " + typeof(string) + "\n" +
+"Boolean is " + typeof(Boolean) + "\n" +
+"Null is " + typeof(Null) + "\n" +
+"undefined is " + typeof(undefined) + "\n" +
+"object is " + typeof(object) + "\n" +
+"symbol is " + typeof(symbol) + "\n" +
+"myFunction is " + typeof(myFunction));
