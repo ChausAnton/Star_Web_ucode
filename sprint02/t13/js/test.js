281743e5a555999d9a9bc23c9ad897538a7fd96d
Anton Chaus <anchaus@c1r6p8.khpi.ucode-connect.study>
2021-03-09 15:00:36 +0200

    t13 fix

--- sprint02/t13/js/test.js
+++
@@ -1,7 +0,0 @@
-const calc = new Calculator();
-console.log(calc.init(2).res);
-console.log(calc.add(2).res);
-console.log(calc.mul(3).res);
-console.log(calc.div(4).res);
-console.log(calc.sub(2).res);
-calc.alert();
