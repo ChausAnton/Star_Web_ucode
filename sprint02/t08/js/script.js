947eb4fbb34fb76d422b81e283391e9579a4647f
Anton Chaus <anchaus@c1r6p8.khpi.ucode-connect.study>
2021-03-09 12:48:41 +0200

    t08 fix

--- sprint02/t08/js/script.js
+++ sprint02/t08/js/script.js
@@ -1,5 +1,6 @@
 function checkBrackets(str) {
     if(typeof(str) == "string") {
+        let cnt = 0;
         let i = 0;
         let open = 0;
         let need = 0;
@@ -9,15 +10,19 @@ function checkBrackets(str) {
             }
             else if(str[i] == ')') {
                 if(open == 0) {
+                    cnt++;
                     need++;
                 }
                 else {
+                    cnt++;
                     open--;
                 }
             }
             i++
         }
-        return need + open;
+        if(cnt > 0) {
+            return need + open;
+        }
     }
     return -1;
 }
--- sprint02/t08/js/test.js
+++ sprint02/t08/js/test.js
@@ -1,2 +1,18 @@
-console.log(checkBrackets('1)()(())2(()'));// 2
-console.log(checkBrackets(NaN));// -1
+const expect = mocha.chai.expect
+
+expect(checkBrackets(undefined)).to.equal(-1)
+expect(checkBrackets(null)).to.equal(-1)
+expect(checkBrackets(NaN)).to.equal(-1)
+expect(checkBrackets(42)).to.equal(-1)
+expect(checkBrackets(42n)).to.equal(-1)
+expect(checkBrackets(true)).to.equal(-1)
+expect(checkBrackets('empty')).to.equal(-1)
+expect(checkBrackets(')')).to.equal(1)
+expect(checkBrackets('))()')).to.equal(2)
+expect(checkBrackets('1)()(())2(()')).to.equal(2)
+
+expect(checkBrackets('((2)))4)(())5)')).to.not.equal(2)
+expect(checkBrackets('((1)((6)())')).to.not.equal(2)
+expect(checkBrackets('1)())())(())2(()7)')).to.not.equal(2)
+expect(checkBrackets('((()())))(4))7(((8))')).to.not.equal(2)
+expect(checkBrackets('()((8((6)((7)(3))))')).to.not.equal(2)
