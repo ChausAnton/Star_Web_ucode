0f6065339836c3b17fef63749bd41cd4fb4c37b5
Anton Chaus <anchaus@c1r7p8.khpi.ucode-connect.study>
2021-03-12 19:21:18 +0200

    t05 start

---
+++ sprint03/t05/js/script.js
@@ -0,0 +1,10 @@
+let guestList = new WeakSet();
+
+guetList.add({name: "name1"});
+guetList.add({name: "name2"});
+guetList.add({name: "name3"});
+guetList.add({name: "name4"});
+
+let menu = new Map();
+//WeakSet();
+//Set();
