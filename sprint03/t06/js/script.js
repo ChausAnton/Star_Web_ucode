65eeb6c5b99fc84ef87e99a7bdc8196a9f054bcc
Anton Chaus <anchaus@c1r7p7.khpi.ucode-connect.study>
2021-03-13 13:27:34 +0200

    t06

--- sprint03/t06/js/script.js
+++ sprint03/t06/js/script.js
@@ -1,77 +1,17 @@
-let guestList = new WeakSet();
+let res = 1;
+while(true) {
+    let num = prompt("Previous result: " + res + ". Enter a ne number:");
 
-console.log("guestList");
-guestList.add({name: "name1"});
-guestList.add({name: "name2"});
-guestList.add({name: "name3"});
-guestList.add({name: "name4"});
-console.log(guestList);
-
-console.log("menu");
-
-let menu = new Map();
-menu = ([
-    ["Jollof Rice", "60"],
-    ["Egusi Soup", "30"],
-    ["Akara", "55"],
-    ["Moi Moi", "56"]
-])
-
-for(let i of menu) {
-    console.log(menu);
-}
-
-console.log("bankVault");
-
-let bankVault = new WeakMap();
-
-let an = {
-    name: "an",
-    credentials: {login: "Anch", password: "0000"},
-    content: "pogchamp"
-};
-
-let ch = {
-    name: "ch",
-    credentials: {login: "cha", password: "1111"},
-    content: "pogchamp1"
-};
-
-bankVault.set(an);
-bankVault.set(ch);
-
-console.log(an);
-
-console.log("coinCollection");
-
-let coinCollection = new Set();
-
-coinCollection.add({
-    name: "Lincoln Cent",
-    Collecting_Grade: "EF-40",
-    Investment_Grade: "MS-65"
-});
-
-coinCollection.add({
-    name: "Peace Dollar",
-    Collecting_Grade: "MS-63",
-    Investment_Grade: "MS-65"
-});
-
-coinCollection.add({
-    name: "Morgan Silver Dollar",
-    Collecting_Grade: "MS-63",
-    Investment_Grade: "MS-65"
-});
-
-coinCollection.add({
-    name: "Buffalo Nickel",
-    Collecting_Grade: "EF-40",
-    Investment_Grade: "MS-63"
-});
-
-for(let i of coinCollection) {
-    console.log(i.name);
-    console.log(i.Collecting_Grade);
-    console.log(i.Investment_Grade);
+    if(isNaN(num)) {
+        console.error("Invalid number!");
+        continue;
+    }
+    num = 1 * num;
+    if(num == 56){
+        break;
+    }
+    res += num
+    if(res > 10000) {
+        res = 1;
+    }
 }
