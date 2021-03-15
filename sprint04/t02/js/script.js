8d38803e33965fc7b8b697d19749331ed3913c41
Anton Chaus <anchaus@c1r7p8.khpi.ucode-connect.study>
2021-03-15 16:53:25 +0200

    t02 set tabel

--- sprint04/t02/js/script.js
+++ sprint04/t02/js/script.js
@@ -1,5 +1,5 @@
+var Top = new Array('Name', 'Strength', 'Age');
 var content = [
-    ["Name", "Strength", "Age"],
     ["Black Panther", "66", "53"],
     ["Capitain America", "79", "137"],
     ["Capitan Marvel", "97", "26"],
@@ -9,4 +9,30 @@ var content = [
     ["Thanos", "99", "1000"],
     ["Thor", "95", "1000"],
     ["Yon-Rogg", "73", "52"]
-]
+];
+
+function setTable() {
+    let body = document.getElementsByTagName("body")[0];
+    let table = document.createElement("table");
+    let tbody = document.createElement("tbody");
+    for(let i = 0; i < 10; i++) {
+        let row = document.createElement("tr");
+        for(let j = 0; j < 3; j++) {
+            let cell = document.createElement("td");
+            if(i == 0) {
+                let text = document.createTextNode(Top[j]);
+                cell.appendChild(text);
+            }
+            else {
+                let text = document.createTextNode(content[i - 1][j]);
+                cell.appendChild(text);
+            }
+            row.appendChild(cell);
+        }
+        tbody.appendChild(row);
+    }
+    table.appendChild(tbody);
+    body.appendChild(table);
+}
+
+setTable();
