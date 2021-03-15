910de548b664a54a0913588e7259c1da02e86425
Anton Chaus <anchaus@c1r7p8.khpi.ucode-connect.study>
2021-03-15 15:13:20 +0200

    t01

---
+++ sprint04/t01/js/script.js
@@ -0,0 +1,38 @@
+let elements = document.querySelectorAll("#characters > li");
+
+for(let elem of elements) {
+    if(elem != null) {
+        if(elem.classList != "good" && elem.classList != "evil" && elem.classList != "unknown") {
+            elem.classList = "unknown";
+        }
+        if(elem.getAttribute("data-element") == null) {
+            elem.setAttribute("data-element", "none");
+        }
+
+        if(elem.getAttribute("data-element") == "none") {
+            elem.appendChild(document.createElement("br"));
+
+            let newDiv = document.createElement("div");
+            newDiv.setAttribute("data-element", elem.getAttribute("data-element"))
+            newDiv.setAttribute("class", "elem");
+            let line = document.createElement("div");
+            line.setAttribute("class", "line");
+            newDiv.appendChild(line);
+            elem.appendChild(newDiv);
+        }
+        else {
+            elem.appendChild(document.createElement("br"));
+            for(let i of elem.getAttribute("data-element").split(" ")) {
+                let newDiv = document.createElement("div");
+                newDiv.setAttribute("data-element", elem.getAttribute("data-element"))
+                newDiv.setAttribute("class", i + " " + "elem");
+                elem.appendChild(newDiv);
+                console.log(newDiv);
+            }
+        }
+    }
+}
+
+for(let i of elements) {
+    console.log(i);
+}
