695b872df15403984c6694d639e4554631122d6f
Anton Chaus <anchaus@c1r7p8.khpi.ucode-connect.study>
2021-03-18 15:11:23 +0200

    t01

--- sprint04/t01/js/script.js
+++ sprint04/t01/js/script.js
@@ -6,19 +6,21 @@ for(let elem of elements) {
         if(elem.classList != "good" && elem.classList != "evil" && elem.classList != "unknown") {
             elem.classList = "unknown";
         }
-        if(elem.getAttribute("data-element") == null) {
+        if(!elem.getAttribute("data-element")) {
             elem.setAttribute("data-element", "none");
         }
 
         if(elem.getAttribute("data-element") == "none") {
+            console.log("adsf ");
             elem.appendChild(document.createElement("br"));
 
             let newDiv = document.createElement("div");
-            newDiv.setAttribute("data-element", elem.getAttribute("data-element"))
+            newDiv.setAttribute("data-element", "none")
             newDiv.setAttribute("class", "elem");
             let line = document.createElement("div");
             line.setAttribute("class", "line");
             newDiv.appendChild(line);
+            elem.appendChild(newDiv);
         }
         else {
             elem.appendChild(document.createElement("br"));
