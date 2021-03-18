84cc1003d6c85b2273748ecf3df167685282d435
Anton Chaus <anchaus@c1r7p8.khpi.ucode-connect.study>
2021-03-18 15:05:20 +0200

    t03

--- sprint04/t03/js/script.js
+++ sprint04/t03/js/script.js
@@ -1,14 +1,6 @@
 
 var slideCount = 1;
-
-autoSlide();
-
-function autoSlide() {
-    setTimeout(() => {
-        showSlide(1);
-        autoSlide();
-    }, 3000);
-}
+var timerID = -1;
 
 function showSlide(i) {
     if (slideCount == 1 && i == -1) {
@@ -19,7 +11,13 @@ function showSlide(i) {
 
     let image = document.querySelector(".main_box .image_box .image");
     image.setAttribute("src", "assets/images/super" + slideCount + ".png");
+
+    clearTimeout(timerID);
+    timerID = setTimeout(showSlide, 3000, 1);
 }
+
+
+timerID = setTimeout(showSlide, 3000, 1);
 
 document.querySelector(".main_box .left").addEventListener("click", event => {
     showSlide(-1);
