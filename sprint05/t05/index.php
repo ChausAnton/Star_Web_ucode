531285b052969cd936ad826d402a8dd9ce08bb21
Anton Chaus <anchaus@c1r7p7.khpi.ucode-connect.study>
2021-03-23 16:37:47 +0200

    t05

--- sprint05/t05/index.php
+++ sprint05/t05/index.php
@@ -2,11 +2,18 @@
     class StrFrequencies {
         
         function __construct($str) {
-            $this->str = $str;
+            for($i = 0; $i < strlen($str); $i++) { 
+                if(ctype_alpha($str[$i]) || $str[$i] == " ") {
+                    $this->str = $this->str . $str[$i];
+                }
+                else {
+                    $this->str = $this->str . " ";
+                }
+            }
         }
 
         function letterFrequencies() {
-            $temp = count_chars($this->str, 1);
+            $temp = count_chars(strtolower($this->str), 1);
             $res = [];
             foreach($temp as $i => $v) {
                 if(ctype_alpha(chr($i))) {
@@ -16,6 +23,26 @@
             ksort($res);
 
             return $res;
+        }
+
+        function wordFrequencies() {
+            $temp_str = "";
+            for($i = 0; $i < strlen($this->str); $i++) { 
+                if(ctype_alpha($this->str[$i]) || $this->str[$i] == " ") {
+                    $temp_str = $temp_str . $this->str[$i];
+                }
+            }
+
+            $tmp = array_count_values(str_word_count(strtolower($temp_str), 1));
+            $res = [];
+            foreach($tmp as $i => $v) {
+                $res[strtoupper($i)] = $v;
+            }
+            return $res;
+        }
+
+        function reversString() {
+            return strrev($this->str);
         }
     }
 
--- sprint05/t05/test.php
+++ sprint05/t05/test.php
@@ -7,7 +7,21 @@
         foreach($symbol as $k => $v) {
             echo "Letter " . $k . " is repeated " . $v . " times\n";
         }
+
+        $symbol = $obj->wordFrequencies();
+        echo "Word in " . $str . "\n";
+        foreach($symbol as $k => $v) {
+            echo "Word " . $k . " is repeated " . $v . " times\n";
+        }
+
+        echo "Revers the string: " . $str . "\n";
+        echo $obj->reversString() . "\n";
+       
     }
 
-    test("Face it, Harley--- you and your Puddin' are kaput!")
+    test("Face it, Harley-- you and your Puddin' are kaput!");
+    echo("***************\n");
+    test("  Test test 123 45 !0 f    HeLlO wOrLd  ");
+    echo("***************\n");
+    test("");
 ?>
