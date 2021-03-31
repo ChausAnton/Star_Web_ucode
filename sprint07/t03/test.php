4bdc5e954733bf6c3124b64d5d49af47dcf22e0b
Anton Chaus <anchaus@c1r7p8.khpi.ucode-connect.study>
2021-03-31 18:06:02 +0300

    t03

--- sprint07/t03/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-    include_once ("Ant.php");
-    $ant = new Ant("Anthony", "sergeant", "2015-07-16", 1, 4);
-    $serialized = serialize($ant);
-    echo $serialized . "\n\n";
-    
-    $unserialized = unserialize($serialized);
-    echo $unserialized;
-?>
