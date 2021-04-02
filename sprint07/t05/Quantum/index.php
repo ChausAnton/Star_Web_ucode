e78fbdee08a599847f6c831612dac326189d1bf1
ChausAnton <antoncaus@MacBook-Air-Anton.local>
2021-04-02 15:49:12 +0300

    t05

--- sprint07/t05/Quantum/index.php
+++
@@ -1,6 +0,0 @@
-<?php namespace Space\Quantum;
-    function calculate_time() : array {
-        return [11, 11, 30];
-    }
-
-?>
--- sprint07/t05/index.php
+++ sprint07/t05/index.php
@@ -1,11 +1,81 @@
 <?php
-    require_once __DIR__ . '/Normal/index.php';
-    require_once __DIR__ . '/Quantum/index.php';
-    $time = Space\Normal\calculate_time();
-    echo "In real life you were absent for " . $time->format("%Y") . " years, " .
-        $time->format("%m") . " months, " . $time->format("%d") . " days\n";
-        
-    $time = Space\Quantum\calculate_time();
-    echo "\nIn quantum space you were absent for " . $time[0] . " years, " .
-        $time[1] . " months, " . $time[2] . " days\n";
+    session_start();
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Parsing CSV data</h1>
+    <form action="" method="POST">
+        <span>Upload file: <input type="file" name="CSV"></span>
+        <input type="submit" value="Upload" name="upload" id="fileSelect">
+    </form>
+    <form action="" method="POST" style="padding-top: 20px;">
+        <span>Filter: 
+            <select name="filter_select[]" size="1">
+                <?php
+                    $filters = ['NOT SELECTED', 'good', 'bad', '-', 'neutral'];
+                    foreach($filters as $i) {
+                        if(isset($_SESSION["filter"]) && $_SESSION["filter"] == $i) {
+                            echo "<option selected>" . $_SESSION["filter"] . "</option>";
+                        }
+                        else {
+                            echo "<option>" . $i . "</option>";
+                        }
+                    }
+                    if(isset($_POST['apply'])) {
+                        header("Refresh:0");
+                    }
+                ?>
+            </select>
+        </span>
+        <input type="submit" name="apply" value="APPLY">
+    </form>
+    <?php
+        if(isset($_POST['filter_select'])) {
+            $_SESSION["filter"] = $_POST['filter_select'][0];
+        }
+
+        if(isset($_POST["upload"]) && isset($_POST['CSV']) && !isset($_POST['apply'])) {
+            $_SESSION["csv_file"] = $_POST['CSV'];
+        }
+        if(isset($_SESSION["csv_file"])) {
+            $csv_open = fopen($_SESSION["csv_file"], "r");
+
+            echo '<table border="1">';
+
+            if($csv_line = fgetcsv($csv_open)) {
+                echo '<tr>';
+                foreach($csv_line as $str) {
+                    echo '<th>' . $str . '</th>';
+                }
+                echo '</tr>';
+            }
+
+            while($csv_line = fgetcsv($csv_open)) {
+                if(isset($_SESSION["filter"]) && $_SESSION["filter"] != "NOT SELECTED") {
+                    if($_SESSION["filter"] != $csv_line[2]) {
+                        continue;
+                    }
+                }
+
+                echo '<tr>';
+                foreach($csv_line as $str) {
+                    echo '<td>' . $str . '</td>';
+                }
+                echo '</tr>';
+            }
+            
+            echo '</table>';
+
+            fclose($csv_open);
+        }
+    ?>
+</body>
+</html>
