a67f68d78b746643f5f25275d98918e27c08457d
Anton Chaus <anchaus@c1r7p8.khpi.ucode-connect.study>
2021-04-03 16:39:00 +0300

    t09

--- sprint07/t09/index.php
+++ sprint07/t09/index.php
@@ -4,47 +4,66 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 <body>
-    <h1>New Avenger application</h1>
-    <div class="look_post" style="width: 100%; border: 2px solid black; background: #C0C0C0;">
-        <h2>POST</h2>
-        <?php
+    <h1>Comics from Marvel API</h1>
 
-            $arr = [
-                "name" => $_POST["name"],
-                "email" => $_POST["email"],
-                "age" => $_POST["age"],
-                "description" => $_POST["description"],
-                "photo" => $_POST["photo"]
-            ];
-            
-            if($arr["name"]) {
-                
-                echo "<pre>";
-                print_r ($arr);
-                echo "</pre>";
+    <?php
+        $timeStamp = time();
+        $API_BASE = "http://gateway.marvel.com/v1/public/comics?";
+        $PUBLIC_KEY = "9e6bffa95ace26b8f33aa51422c7b754";
+        $PRIVATE_KEY = "f86aa9046964cb1e3a1053660272f4b93d513e4c";
+        $HASH = md5($timeStamp . $PRIVATE_KEY . $PUBLIC_KEY);
+        $api = $API_BASE . "ts=".  $timeStamp . "&apikey=" . $PUBLIC_KEY . "&hash=" . $HASH;
+
+        $cURL = curl_init();
+        curl_setopt($cURL, CURLOPT_URL, $api);
+        curl_setopt($cURL, CURLOPT_RETURNTRANSFER, 1);
+        $json = curl_exec($cURL);
+        curl_close($cURL);
+        echo(parseJSON(json_decode($json, true)));
+        //$json = json_decode($json, true);
+        $out = '<div class="block">';
+
+        
+        function parseJSON($json) {
+            $out = '<div class="block">';
+            foreach($json as $i => $v) {
+                $out .= '<div>';
+                if(is_array($v)) {
+                    $out .= '<span class="header"><br>' . $i . ': </span>';
+                    $out .= parseJSON($v);
+                }
+                else {
+                    $out .= '<div><span class="key">' . $i . ': </span><span class="value">' . $v . '</span></div>';
+                }
+                $out .= '</div>';
             }
+            $out .= '</div>';
+            return $out;
+        }
 
-        ?>
-    </div>
-    <form action="index.php" method="post" class="form_box"  style="border: 2px solid gray; padding: 20px;">
-        <div class="firs_data" style="border: 2px solid gray; padding: 20px 20px 20px 20px;">
-            <p class="About_the_superhero" style=" position: relative;background-color: white;bottom: 45px;margin-bottom: -30px;width: 135px;">About the Superhero</p>
-            <span>Name</span> 
-            <input type="text" class="real_name" placeholder="Tell your name" name="name"> 
-            <span>E-mail</span>      
-            <input type="text" class="Superhero name" placeholder="Tell your E-mail" name="email">
-            <span>Age</span>
-            <input type="number" class="age" min="1" max="999" name="age">
-            <br><span>About</span>
-            <textarea rows="5" cols="70" class="about" maxlength="500" placeholder="Tell about yourself, max 500 symbols" name="description"></textarea><br><br>
-            <span>Photo </span> <input type="file" name="photo">
-        </div>
-        <input type="reset" value="CLEAR" class="reset">
-        <input type="submit" value="SEND" class="send">
-    </form>
+        /*function parseJSON($json) {
+            $output = "<div class=\"block\">";
+            foreach ($json as $key => $value) {
+                if (is_array($value)) {
+                    $output .= "<span class=\"header\"><br>$key:</span>";
+                    $output .= parseJSON($value);
+                } 
+                else {
+                    $output .= "
+                        <div>
+                            <span class=\"key\">$key: </span>
+                            <span class=\"value\">$value</span>
+                        </div>
+                    ";
+                }
+            }
+            $output .= "</div>";
+            return $output;
+        }*/
+    ?>
 </body>
 </html>
-
