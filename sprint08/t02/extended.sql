a33aea9b7a3440c441e9665bdb6ddbb8b376cacd
Anton Chaus <anchaus@c1r7p8.khpi.ucode-connect.study>
2021-04-05 16:23:41 +0300

    t02

--- sprint08/t02/extended.sql
+++ sprint08/t02/extended.sql
@@ -1,3 +1,62 @@
-create database ucode_web;
-create user 'anchaus'@'localhost' identified by 'securepass';
-grant all privileges on ucode_web . * to anchaus@localhost;
+USE ucode_web;
+
+CREATE TABLE IF NOT EXISTS powers (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    hero_id INT not NULL,
+    name VARCHAR(30) not NULL,
+    points INT not NULL,
+    type ENUM('attack', 'defense'),
+
+    FOREIGN KEY(hero_id) REFERENCES heroes(id)
+);
+
+CREATE TABLE IF NOT EXISTS race (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    hero_id INT not NULL,
+    name VARCHAR(30),
+
+    FOREIGN KEY(hero_id) REFERENCES heroes(id)
+);
+
+CREATE TABLE IF NOT EXISTS teams (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    hero_id INT not NULL,
+    name VARCHAR(30),
+
+    FOREIGN KEY(hero_id) REFERENCES heroes(id)
+);
+
+INSERT INTO powers (hero_id, name, points, type) values (
+    1,
+    'bloody fist',
+    110,
+    'attack'
+);
+
+INSERT INTO powers (hero_id, name, points, type) values (
+    2,
+    'efense',
+    110,
+    'defense'
+);
+
+INSERT INTO race (hero_id, name) values (
+    3,
+    'Human'
+);
+
+INSERT INTO race (hero_id, name) values (
+    4,
+    'Kree'
+);
+
+INSERT INTO teams (hero_id, name) values (
+    5,
+    'Avengers'
+);
+
+INSERT INTO teams (hero_id, name) values (
+    5,
+    'Hydra'
+);
+
