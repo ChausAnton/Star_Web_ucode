2ee155f4fa04b8af368f33ef6e819dc17c480642
Anton Chaus <anchaus@c1r7p8.khpi.ucode-connect.study>
2021-04-05 19:52:48 +0300

    t05

--- sprint08/t05/find_me.sql
+++ sprint08/t05/find_me.sql
@@ -4,4 +4,4 @@ SELECT heroes.name FROM heroes
 INNER JOIN teams ON heroes.id = teams.hero_id
 WHERE race != "human" AND heroes.name LIKE BINARY '%a%' AND class_role = 'tankman' or class_role = 'healer' 
 GROUP BY teams.hero_id HAVING COUNT(teams.hero_id) > 1
-ORDER BY heroes.id DESC LIMIT 1;
+ORDER BY heroes.id ASC LIMIT 1;
