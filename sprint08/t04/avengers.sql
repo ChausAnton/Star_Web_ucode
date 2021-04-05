843b908bd15747f1b597cb292989afe9574dee8d
Anton Chaus <anchaus@c1r7p8.khpi.ucode-connect.study>
2021-04-05 17:20:39 +0300

    t04

--- sprint08/t04/avengers.sql
+++ sprint08/t04/avengers.sql
@@ -4,3 +4,9 @@ SELECT total.hero_id, total.points from (
     SELECT powers.hero_id, SUM(powers.points) AS points
     FROM powers GROUP BY powers.hero_id
 ) as total ORDER BY total.points DESC LIMIT 1;
+
+SELECT total.hero_id, total.points from (
+    SELECT powers.hero_id, SUM(powers.points) AS points
+    FROM powers WHERE powers.type = 'defense'
+    GROUP BY powers.hero_id
+) as total ORDER BY total.points ASC LIMIT 1;
